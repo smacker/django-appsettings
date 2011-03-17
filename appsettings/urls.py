9bae11ab2caf021998b0d8670d61719c49703ba9
Maxim Sukharev <max@smacker.ru>
2011-03-17 13:43:21 +0500

    default admin templates

--- appsettings/urls.py
+++ appsettings/urls.py
@@ -5,8 +5,9 @@ admin.autodiscover()
 import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.app_index, name='index'),
+        url(r'^$', views.app_index, name='app_index'),
         url(r'^(?P<app_name>[^/]+)/$', views.app_settings, name='app_settings'),
+        url(r'^(?P<app_name>[^/]+)/(?P<group_name>[^/]+)/$', views.app_group_settings, name='app_group_settings'),
     )
 
 # vim: et sw=4 sts=4
--- appsettings/views.py
+++ appsettings/views.py
@@ -7,16 +7,54 @@ settingsinst = Settings()
 from models import Setting
 import forms
 
+def get_apps(settingsinst):
+    app_list = []
+    apps = vars(settingsinst).items()
+    for name, app in apps:
+        groups = []
+        for group_name, group in app._vals.iteritems():
+            if group._readonly:continue
+            groups.append([group_name, group])
+        app_list.append([name, groups])
+    return app_list
+
+
 @staff_member_required
 def app_index(request, template = 'appsettings/index.html', base_template = 'index.html'):
-    apps = sorted(vars(settingsinst).keys())
+    apps = get_apps(settingsinst)
     return render_to_response(template, 
-            {'apps':apps, 'base_template':base_template},
+            {'app_list':apps, 'base_template':base_template},
             RequestContext(request))
 
 @staff_member_required
-def app_settings(request, app_name=None, template = 'appsettings/settings.html', base_template = 'index.html'):
-    editor = forms.settings_form()
+def app_settings(request, app_name=None, template = 'appsettings/index.html', base_template = 'index.html'):
+    apps = []
+    groups = []
+    app = getattr(settingsinst, app_name)
+    for group_name, group in app._vals.iteritems():
+        if group._readonly:continue
+        groups.append([group_name, group])
+    apps.append([app_name, groups])
+    return render_to_response(template, 
+            {'app_list':apps, 'base_template':base_template, 'app_name':app_name},
+            RequestContext(request))
+
+def app_group_settings(request, app_name, group_name, template = 'appsettings/settings.html', base_template = 'index.html'):
+    fields = {}
+    fieldsets = []
+    initial = {}
+
+    app = getattr(settingsinst, app_name)
+    group = app._vals[group_name]
+    fieldset_fields = []
+    for key, value in group._vals.iteritems():
+        field_name = u'%s-%s-%s' % (app_name, group_name, key)
+        fields[field_name] = value
+        fieldset_fields.append(field_name)
+        initial[field_name] = getattr(group, key)
+    fieldsets.append((group_name, group._verbose_name, fieldset_fields,))
+    editor = type('SettingsForm', (forms.FieldsetForm,),
+                     {'base_fieldsets': fieldsets, 'base_fields':fields})
     if request.POST:
         form = editor(request.POST)
         if form.is_valid():
@@ -26,15 +64,7 @@ def app_settings(request, app_name=None, template = 'appsettings/settings.html',
                 if val != getattr(settingsinst, app)._vals[group]._vals[name].initial:
                     setattr(getattr(settingsinst, app)._vals[group], name, val)
     else:
-        initial = {}
-        for app_name in sorted(vars(settingsinst).keys()):
-            app = getattr(settingsinst, app_name)
-            for group_name, group in app._vals.iteritems():
-                if group._readonly:continue
-                for key, value in group._vals.iteritems():
-                    field_name = u'%s-%s-%s' % (app_name, group_name, key)
-                    initial[field_name] = getattr(group, key)
         form = editor(initial)
-    return render_to_response(template, 
-                              {'app':app_name,'form':form, 'base_template':base_template},
+    return render_to_response(template,
+                              {'app':app_name, 'group':group_name, 'form':form, 'base_template':base_template},
                               context_instance=RequestContext(request))
